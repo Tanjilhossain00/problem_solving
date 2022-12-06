cc8425c5a7ab362300c0192e5148b0d6d6c2ef34
TANJIL HOSSAIN <118347892+Tanjilhossain00@users.noreply.github.com>
2022-12-06 15:23:10 +0600

    Add files via upload

---
+++ conditionsolving.py
@@ -0,0 +1,106 @@
+# solving no:1
+
+# salary=float(input('Enter your salary:'))
+# job_age=int(input('Enter your service year:'))
+# bonous = salary * (5 / 100)
+# if job_age>=5:
+#     print("your bonous amount is: ",bonous)
+# elif job_age>=10:
+#     print('Your bonous amount is:',(salary*(10/100)))
+# else:
+#     print("no bonous")
+
+##solving no:2
+# r_length=float(input('Enter length:'))
+# r_weidth=float(input("Enter width:"))
+# if r_weidth==r_length:
+#     print('it is a squar')
+# else:
+#     print('it is a rectangular')
+
+##solving no:3
+# i=int(input('number:'))
+# j=int(input('number:'))
+#
+# if i>j:
+#     print('Max number is',i)
+# elif j>i:
+#     print('max number is:',j)
+# else:
+#    print('Both number is same')
+
+
+
+##solving no:3
+
+# quantity=int(input("Enter your buy quantity:"))
+# cost=quantity*100
+# discount=cost*(10/100)
+# total_price=cost-discount
+# if cost>=1000:
+#     print('you got 10% discount your finel price is',total_price)
+#     print('You save',discount,'taka')
+# else:
+#     print('you dont have any discount ,your regular price is',cost,'Taka')
+
+##solving no:4
+#
+# marks=int(input('Enter your marks:'))
+# if marks<25:
+#     print('F')
+# elif marks>=25 and marks<45:
+#     print('D')
+# elif marks>=45 and marks<50:
+#     print('C')
+# elif marks>=50 and marks<60:
+#     print('B')
+# elif marks>=60 and marks<80:
+#     print('A')
+# else:
+#     print('A+')
+#
+##solving no:5
+# first=int(input('Age:'))
+# second=int(input('Age:'))
+# third=int(input('Age:'))
+# if first>=second and first>=third:
+#     print('Oldest',first)
+#     print('Youngest',min(first,second,third))
+# elif second>=first and second>=third:
+#     print('Oldest',second)
+#     print('Youngest', min(first, second, third))
+# elif third>=first and third>=second:
+#     print('Oldest',third)
+# else:
+#     print('All are equal')
+#problem solving 6
+# price=int(input('price:''Tk'))
+# discount=int(input('discount%:'))
+# total=price-(price*(discount/100))
+# save=price*(discount/100)
+# print('Your discount price is:',total,'Tk')
+# print('You save ',save,'Tk')
+
+#problem solving 7
+
+# number_of_class_held=int(input('Number of class held:'))
+# attendence=int(input('Number of class Attendece day:'))
+# print('Did You have madical case?')
+# madical_case=input('Y/N: ')
+# attendence_rate=(attendence/number_of_class_held)*100
+# print('Attendece rate',round(attendence_rate),'%')
+#
+# if attendence_rate>74:
+#     print('You are Allowed  in the exam')
+# elif attendence_rate<75 and madical_case=="y":
+#     print('You had a madical case')
+#     print('So you are allowed in the exam')
+# else:
+#     print("You aren't Allow in The exam")
+
+#problem solving 8
+
+
+
+
+
